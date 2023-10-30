3b739d39588ee3ea1ed0cf5282591d7d37f382e6
Volodymyr Skvira <deruty13061999@gmail.com>
2023-10-30 11:22:16 +0200

    Added "EntityGraph" annotation

--- src/main/java/s/volodymyr/onlinebookstore/repository/user/UserRepository.java
+++ src/main/java/s/volodymyr/onlinebookstore/repository/user/UserRepository.java
@@ -1,9 +1,11 @@
 package s.volodymyr.onlinebookstore.repository.user;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import s.volodymyr.onlinebookstore.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
 }
