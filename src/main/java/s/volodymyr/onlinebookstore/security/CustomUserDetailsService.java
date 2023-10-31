7c9112f925128c05c70ee7c4aa57fda8bcd441b3
Volodymyr Skvira <deruty13061999@gmail.com>
2023-10-31 19:15:03 +0200

    Removed redundant Component annotation

--- src/main/java/s/volodymyr/onlinebookstore/security/CustomUserDetailsService.java
+++ src/main/java/s/volodymyr/onlinebookstore/security/CustomUserDetailsService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import s.volodymyr.onlinebookstore.repository.user.UserRepository;
 
